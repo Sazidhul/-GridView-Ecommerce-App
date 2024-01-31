fdad04b2d2b01d7815339744d87583ceff1d34d2
Sazidhul <154440493+Sazidhul@users.noreply.github.com>
2024-01-31 19:17:22 +0600

    This app is a Ecommerce

---
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = findViewById(R.id.gridView);
+
+
+        MyAdapter adapter = new MyAdapter();
+        gridView.setAdapter(adapter);
+
+    }
+
+    ///===========================
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View myView = layoutInflater.inflate(R.layout.item, viewGroup,false);
+            return myView;
+        }
+    }
+
+
+}
